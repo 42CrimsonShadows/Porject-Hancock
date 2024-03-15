35f91db2cfa49f78f3bb401c03f086a64b2ef813
Chase Franklin <82984795+submeonic@users.noreply.github.com>
2024-03-15 10:48:14 -0700

    Menu Fixed + Names Dropdown

--- Authentication/ManagerSignInViewController.swift
+++ Authentication/ManagerSignInViewController.swift
@@ -13,13 +13,52 @@ class ManagerSignInViewController: UIViewController {
     @IBOutlet weak var EnterPin: UITextField!
     @IBOutlet weak var ManagerName: UIButton!
     
-    @IBAction func SelectManagerPressed(_ sender: Any) {
-        print("Attempting to Select Manager")
-        //TODO pop up manager list received from service.swift
+    var ManagerNames = [
+        "Option 1",
+        "Option 2",
+        "Option 3",
+        "Option 4",
+        "Option 5",
+        "Option 6",
+        "Option 7",
+        "Option 8",
+        "Option 9",
+        "Option 10",
+        "Option 11",
+    ]
+    
+    @IBAction func SelectManagerPressed(_ sender: UIButton) {
+        let alertController = UIAlertController(title: "Select Item", message: nil, preferredStyle: .actionSheet)
+        let attributes: [NSAttributedString.Key: Any] = [
+            .font: UIFont(name: "Irish Grover", size: 50) ?? UIFont.systemFont(ofSize: 50),
+            .foregroundColor: UIColor.white
+        ]
+        //Add Manager Name Options
+        ManagerNames.forEach { managerName in
+                alertController.addAction(UIAlertAction(title: managerName, style: .default, handler: { [weak self] _ in
+                    guard let self = self else { return }
+                    let attributedString = NSAttributedString(string: managerName, attributes: attributes)
+                    self.ManagerName.setAttributedTitle(attributedString, for: .normal)
+                }))
+            }
+
+        // Include a Cancel action
+        alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+
+        // For iPad compatibility
+        if let popoverController = alertController.popoverPresentationController {
+            popoverController.sourceView = sender
+            popoverController.sourceRect = sender.bounds
+        }
+
+        // Present the alertController
+        present(alertController, animated: true)
     }
     
     @IBAction func LoginPressed(_ sender: Any) {
-        Service().AttemptLogin(username: ManagerName.titleLabel!.text!, pin: EnterPin.text!)
+        
+        //print("username: " + ManagerName.attributedTitle(for: .normal) + " Pin: " + EnterPin.text!)
+        //Service().AttemptLogin(username: ManagerName.attributedTitle(for: .normal), pin: EnterPin.text!)
     }
     override func viewDidLoad() {
         super.viewDidLoad()
